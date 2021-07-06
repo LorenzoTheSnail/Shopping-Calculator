7381e06014d2a3652738b590c2fe241239772d85
LorenzoTheSnail <79881220+LorenzoTheSnail@users.noreply.github.com>
2021-07-06 17:14:32 -0500

    Add files via upload

--- shoppingCalculator.py
+++ shoppingCalculator.py
@@ -1,19 +1,21 @@
 z = 0
 
-#change the number below to whatever your state's sales tax is.
+#change the number below to your state's sales tax is.
 tax = .0625
 
 while 1 > 0:
     x = input('What is the price of your next item? If you are done type Done:')
     if str(x) == str('Done'):
         y = float(y) * float(tax) + float(y)
-        print('Your total is ',y)
+        z = round(y, 2)
+        print('Your total is ',z)
         break
     elif str(x) == str('done'):
-        y = float(y) * .0625 + float(y)
-        print('Your total is $',y)
-        break
-    y = float(z) + float(x)
+     y = float(y) * float(tax) + float(y)
+     z = round(y, 2)
+     print('Your total is $',z)
+     break
+    y = float(z) + float(x)            
     z = y
     x = 0
     print(y)
